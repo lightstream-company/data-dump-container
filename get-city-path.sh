e02260f5a433b9ff3e8077b88f90ecae78ab39bd
Franck Ernewein <franck.ernewein@gmail.com>
2016-09-28 18:19:03 +0200

    fix(shp): conserve all files
      geonames id are lin .dbf files

--- get-city-path.sh
+++ get-city-path.sh
@@ -9,11 +9,5 @@ curl -O http://static.quattroshapes.com/$FILENAME.zip
 mkdir $DIR
 unzip -d $DIR $FILENAME.zip
 
-#keep only .shp file
-rm cityshape/$FILENAME.dbf
-rm cityshape/$FILENAME.shx
-rm cityshape/$FILENAME.cpg
-rm cityshape/$FILENAME.prj 
-
 #rm zip file
 rm $FILENAME.zip
